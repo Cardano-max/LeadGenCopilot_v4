a5c427b4e155945b794c37416c390c4df901ad5e
Your Name <mateeb.ce41ceme@ce.ceme.edu.pk>
2025-06-18 14:28:51 +0500

    render chrome path fixed gmap-20

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import GoogleMapsBusinessScraper from './gmaps_scraper.js';
+import { GoogleMapsBusinessScraper } from './gmaps_scraper.js';
 
 // Load environment variables
 dotenv.config();
