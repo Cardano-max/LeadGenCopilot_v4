7cb0967522bfde3c8ff010db08426f37b1ab3e03
Your Name <mateeb.ce41ceme@ce.ceme.edu.pk>
2025-06-18 13:33:47 +0500

    render chrome path fixed gmap-17

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { GoogleMapsBusinessScraper } = require('./gmaps_scraper.js');
+const GoogleMapsBusinessScraper = require('./gmaps_scraper.js');
 const scraper = new GoogleMapsBusinessScraper();
 
 // Load environment variables
